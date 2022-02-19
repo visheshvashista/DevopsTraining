58ccf95381127dd2fa6ec554221b73f27db48aee
vishesh vashista <vishesh.vashista@gmail.com>
2022-02-19 17:50:34 +0530

    test local exec

--- local-exec.tf
+++ local-exec.tf
@@ -4,3 +4,9 @@ resource "null_resource" "url" {
           command = "echo  'vishesh'"
 	}
 }
+
+resource "null_resource" "create-endpoint" {
+  provisioner "local-exec" {
+    command = "aws --region=${var.aws_region} --secret_key=${secret_key} --access_key=${access_key} ec2 describe-instances"
+  }
+}
