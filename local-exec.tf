36a632c917846e1d11055eb2b56d1066d1b10ea7
visheshvashista <35123843+visheshvashista@users.noreply.github.com>
2022-02-19 23:49:39 +0530

    Update local-exec.tf

--- local-exec.tf
+++ local-exec.tf
@@ -7,7 +7,7 @@ resource "null_resource" "testEcho" {
 
 resource "null_resource" "tilakbuckets" {
   provisioner "local-exec" {
-    command = "aws s3 ls --region=${var.aws_region} >  /tmp/s3list.log "
+    command = "aws s3 ls --region=${var.aws_region} >  s3list.log "
     environment = {
       AWS_ACCESS_KEY_ID = "${var.access_key}"
       AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
