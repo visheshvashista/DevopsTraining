55a616f77159841a10e98b63ef616e08d03c85ae
vishesh vashista <vishesh.vashista@gmail.com>
2022-02-19 19:20:45 +0530

    test

--- local-exec.tf
+++ local-exec.tf
@@ -5,12 +5,25 @@ resource "null_resource" "url" {
 	}
 }
 
-resource "null_resource" "s3" {
+resource "null_resource" "s3buckets" {
   provisioner "local-exec" {
-    command = "aws s3 ls --region=${var.aws_region} "
+    command = "aws s3 ls --region=${var.aws_region} >  ${data.template_file.s3buckets.rendered} "
     environment = {
       AWS_ACCESS_KEY_ID = "${var.access_key}"
       AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
     }
   }
+}
+
+data "template_file" "s3buckets" {
+    template = "/output.log"
+}
+
+data "local_file" "s3_bucketlist" {
+    filename = "${data.template_file.s3buckets.rendered}"
+    depends_on = ["null_resource.s3buckets"]
+}
+
+output "S3-Buckets" {
+    value = "${data.local_file.instance_ips.content}"
 }
