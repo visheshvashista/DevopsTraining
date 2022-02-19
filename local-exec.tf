b0c7feb9270a1cbe22800fb1fc286b46bae8d7ed
vishesh vashista <vishesh.vashista@gmail.com>
2022-02-19 18:28:22 +0530

    test local exec

--- local-exec.tf
+++ local-exec.tf
@@ -7,7 +7,7 @@ resource "null_resource" "url" {
 
 resource "null_resource" "create-endpoint" {
   provisioner "local-exec" {
-    command = "aws ec2 describe-instances --region=${var.aws_region}"
+    command = "export secret_key=aws ec2 describe-instances --region=${var.aws_region} --secret_key=${var.secret_key} --access_key=${var.access_key}"
      environment = {
     }
   }
