08733433cc9897a382fcf7fce9f85bd65b3eb06e
visheshvashista <35123843+visheshvashista@users.noreply.github.com>
2022-02-19 23:57:23 +0530

    Update local-exec.tf

--- local-exec.tf
+++ local-exec.tf
@@ -7,10 +7,30 @@ resource "null_resource" "testEcho" {
 
 resource "null_resource" "tilakbuckets" {
   provisioner "local-exec" {
-    command = "aws s3 ls --region=${var.aws_region} >  s3list.log "
+    command = "aws s3 ls --region=${var.aws_region} >  ${data.template_file.tilakbuckets.rendered} "
     environment = {
       AWS_ACCESS_KEY_ID = "${var.access_key}"
       AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
     }
   }
+}
+
+resource "null_resource" "createfile" {
+  provisioner "local-exec" {
+    command = "touch output.log"  
+  }
+}
+
+data "template_file" "tilakbuckets" {
+    template = "output.log"
+    depends_on = ["null_resource.tilakbuckets"]
+}
+
+data "local_file" "tilak_bucketlist" {
+    filename = "${data.template_file.tilakbuckets.rendered}"
+    depends_on = ["null_resource.tilakbuckets"]
+}
+
+output "S3-Buckets" {
+    value = "${data.local_file.tilak_bucketlist.content}"
 }
